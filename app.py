8e7116e5fbde1a6cf8bbab0949152c7399188684
Juan Elvir <jelvir97@gmail.com>
2023-06-16 20:45:03 -0600

    reqs, User model and db created

---
+++ app.py
@@ -0,0 +1,21 @@
+"""Blogly application."""
+from flask import Flask, request,render_template, redirect,flash,session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+from sqlalchemy import Text, text
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'woohoo!123'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+app.app_context().push()
+connect_db(app)
+
+@app.route('/')
+def home_page():
+    
---
+++ models.py
@@ -0,0 +1,35 @@
+"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+"""
+id pk
+first_name
+last_name
+img_url
+"""
+class User(db.Model):
+    __tablename__ = "users"
+
+    def __repr__(self):
+        return f"<User first_name = {self.firstName} last_name={self.last_name}>"
+    
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    
+    first_name = db.Column(db.String(50),
+                     nullable=False)
+    
+    last_name = db.Column(db.String(50),
+                     nullable=False)
+    
+    last_name = db.Column(db.String(),
+                     nullable=False,
+                     default='https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png')
+    
